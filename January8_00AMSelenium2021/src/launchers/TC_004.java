1ad6b5b924b185e8c055d1f943d6de324418ae96
maha <maha@gmail.com>
2021-02-09 08:55:20 +0530

    Log4j

--- January8_00AMSelenium2021/src/launchers/TC_004.java
+++ January8_00AMSelenium2021/src/launchers/TC_004.java
@@ -1,22 +1,33 @@
 package launchers;
 
+import org.apache.log4j.Logger;
+
 public class TC_004 extends BaseTest
 {
+	private static final Logger log = Logger.getLogger(TC_004.class);
+	
 	
 	public static void main(String[] args) throws Exception 
 	{
+		
 		init();
+		log.info("---------------   Test case Started ----------------------");
+		log.info("Loding Property files & configuration files .......");
 		
 		openBrowser("chromebrowser");
+		log.info("Opened the browser : " + p.getProperty("chromebrowser"));
 		
 		navigateUrl("amazonurl");
+		log.info("Navigated to url : " + childProp.getProperty("amazonurl"));
 		
 		selectOption("amazondropbox_id","Books");
+		log.info("Selected the option Books By using locator :- " + orProp.getProperty("amazondropbox_id"));
 		
 		typeText("amazonsearchtextbox_name","Harry Potter");
+		log.info("Entered the text Harry Potter By using the locator :- " + orProp.getProperty("amazonsearchtextbox_name"));
 		
 		clickElement("amazonsearchbutton_xpath");
-				
+		log.info("Clicked onButton by Using locator :-" + orProp.getProperty("amazonsearchbutton_xpath"));
 	
 		
 		/*
